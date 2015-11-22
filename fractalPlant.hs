f7cd30571af63867938ca38668733271b2c8d325
Angus Lowe <anguslowe@live.ca>
2015-11-22 22:31:17 +0000

    Changed names of files

---
+++ fractalPlant.hs
@@ -0,0 +1,27 @@
+--data Command = Go Distance
+--             | Turn Angle
+--             | Sit
+--             | Command :#: Command
+--             | Branch Command
+--             | GrabPen Pen
+--               deriving (Eq, Ord, Show)
+
+import theLSystem
+import Test.QuickCheck
+
+fractalPlant :: Int -> Command
+fractalPlant x = f x
+               where
+                 f 0 = Sit
+                 f x = g (x-1) :#: n :#: Branch (Branch (f (x-1)) :#: p :#: f (x-1)) :#: p :#: g (x-1) :#: Branch (z :#: g (x-1) :#: f (x-1)) :#: n :#: f (x-1)
+                 g 0 = GrabPen snowblue :#: Go 3 :#: GrabPen white :#: Go 3 :#: GrabPen green :#: Go 2
+                 g x = g (x-1) :#: g (x-1)
+                 p = Turn 60
+                 n = Turn (-60)
+                 z = Turn 60
+
+
+
+
+main :: IO ()
+main = display (fractalPlant 8)
